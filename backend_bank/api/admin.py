4530a18e1fbd8ea565e08d6890d0b5091b4a478f
steve-ongera <gadafisteve001@gmail.com>
2026-07-30 22:16:44 +0300

    made updates

--- backend_bank/api/admin.py
+++ backend_bank/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, IdempotencyKey, Transaction
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("account_number", "user", "balance", "created_at")
+    search_fields = ("account_number", "user__username")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("id", "sender", "receiver", "amount", "status", "created_at")
+    list_filter = ("status",)
+
+
+@admin.register(IdempotencyKey)
+class IdempotencyKeyAdmin(admin.ModelAdmin):
+    list_display = ("key", "user", "status", "request_path", "created_at")
+    list_filter = ("status",)
+    search_fields = ("key",)
